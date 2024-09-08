9d617b3494f0b10f667351e6f8c27d0129726547
Seona <seona_0826@naver.com>
2024-09-08 23:45:11 +0900

    240908_Blog app의 views를 모두 generic class로 교체(CBV: Class Based View)

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
     ordering = '-created_at'
-    
+
     # 기본적으로 post_list.html을 템플릿으로 인식하므로 파일을 rename해주었다.
     # template_name = 'blog/index.html'
 
@@ -16,9 +16,13 @@ class PostList(ListView):
 #         'posts': posts
 #     })
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+class PostDetail(DetailView):
+    model = Post
 
-    return render(request, f'blog/single_post_page.html', {
-        'post': post
-    })
+# 제너릭 뷰 클래스인 DetailView를 상속한 PostDetail class에서 역할을 대신한다.
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(request, f'blog/single_post_page.html', {
+#         'post': post
+#     })
