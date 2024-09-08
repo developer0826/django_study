6ea08b89d38ba41a3e69ccc0f78b02385c5e49bd
Seona <seona0826@gmail.com>
2024-09-08 23:36:20 +0900

    240908_CBV(Class Based View) 찍먹해보기. Blog index를 장고 제너릭 ListView로 대체

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/index.html', {
-        'posts': posts
-    })
+class PostList(ListView):
+    model = Post
+    ordering = '-created_at'
+    
+    # 기본적으로 post_list.html을 템플릿으로 인식하므로 파일을 rename해주었다.
+    # template_name = 'blog/index.html'
+
+# 제너릭 뷰 클래스인 ListView를 상속한 PostList class에서 역할을 대신한다.
+# def index(request):
+#     posts = Post.objects.all().order_by('-created_at')
+#     return render(request, 'blog/index.html', {
+#         'posts': posts
+#     })
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
